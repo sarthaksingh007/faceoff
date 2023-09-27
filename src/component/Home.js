852cdefd32ae843976ecf77437f63e69c20bd5dc
sarthaksingh007 <100351826+sarthaksingh007@users.noreply.github.com>
2023-09-27 13:32:58 +0530

    changes

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
-// import universe from "../images/universe.jpg";
-// import groupimage  from "../images/group.jpg";
-// import astranot from "../images/astranot.jpg";
+import universe from "../assest/universe.png";
+import groupimage  from "../images/group.jpg";
+import astranot from "../assest/astranot.png";
 // import down from "../images/down.jpg"
 export default function Home() {
   return (
     <div className="mt-36 mx-6">
       <div className="px-6">
-        {/* <img src={universe} alt="universeimage" /> */}
+        <img src={universe} alt="universeimage" />
       </div>
        <div className="flex">
       <div className="w-2/4 px-6">
@@ -21,10 +21,10 @@ export default function Home() {
         <div className="flex">
         <button className="bg-black text-white px-6 py-3 mr-6 ml-4 my-6 rounded-lg">Get Started </button>
         <button className="font-semibold">Watch the Video </button>
-        {/* <button className="mx-6"><img className="w-8 h-8" src={groupimage} alt="grpimg" /></button> */}
+        <button className="mx-6"><img className="w-8 h-8" src={groupimage} alt="grpimg" /></button>
         </div>
       </div>
-        {/* <img className=" h-3/4 absolute right-48 top-52 bg-transparent" src={astranot} alt="astranot" /> */}
+        <img className=" h-3/4 absolute right-48 top-52 bg-transparent" src={astranot} alt="astranot" />
         {/* <img className='absolute left-2/4 bottom-6' src={down} alt="down" /> */}
       </div>
     </div>
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
-// import images1 from '../images/logo.jpg'
+import images1 from '../images/logo.jpg'
 export default function Navbar() {
 
   let [open,setOpen]=useState(false);
   return (
-    <div className='w-full fixed top-0 left-0'>
+    <div className='w-full absolute top-0 left-0'>
     <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
     <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-black'>
-      {/* <img src={images1} alt="logoimg" className="h-8 rounded-xl mx-3"/> */}
+      <img src={images1} alt="logoimg" className="h-8 rounded-xl mx-3"/>
     </div>
     
     <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden lg:mb-20'>
